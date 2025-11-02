7bcd393b64119d395e9bbb7f3ec2f9a925b2f29c
HOG <hogseruj1@gmail.com>
2025-11-02 17:04:12 +0200

    Created votes app.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './basic.css'
+import App from './assets/components/App/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
